d8b2de8f37752a9a9fd07028006f7b0efe31db41
sivals8 <sivals8@mcmaster.ca>
2026-01-19 17:25:10 -0500

    Initial set up

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding: '20 px', fontFamily: 'Poppins'}}>
+      <header className="App-header">
+        <h1>Double Cheque</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
